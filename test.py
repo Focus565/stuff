202eb3919cf631dc70b7e18df0b5c99c2389ca42
Kittichet Preedathanaphong <fogus251@hotmail.com>
2017-10-21 10:19:13 +0700

    add hello.py

---
+++ test.py
@@ -0,0 +1,12 @@
+"""GCD"""
+def main():
+    """find gcd"""
+    num1 = int(input())
+    num2 = int(input())
+    num1, num2 = max(num1, num2), min(num1, num2)
+    if num2 == 0:
+        ans = 0
+    for i in range(num2, 0, -1):
+        if num1%i == 0 and num2%i == 0:
+            print(i)
+main()
